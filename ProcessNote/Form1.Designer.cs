2be165aa6e6ca85679e892f8df8c0f6c25bba969
szkrecsi <ecsenszki.judit@gmail.com>
2017-06-15 23:49:26 +0200

    process note

---
+++ ProcessNote/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace ProcessNote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.processTable = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CPU = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.commentBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.processTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.Color.OrangeRed;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.closeButton.Font = new System.Drawing.Font("Papyrus", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeButton.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.closeButton.Location = new System.Drawing.Point(713, 19);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(84, 23);
+            this.closeButton.TabIndex = 4;
+            this.closeButton.Text = "CLOSE";
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // processTable
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.processTable.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.processTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.processTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.CPU,
+            this.Column2});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.processTable.DefaultCellStyle = dataGridViewCellStyle2;
+            this.processTable.Location = new System.Drawing.Point(9, 48);
+            this.processTable.Name = "processTable";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.processTable.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.processTable.Size = new System.Drawing.Size(444, 415);
+            this.processTable.TabIndex = 5;
+            this.processTable.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.processTable_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Process";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 200;
+            // 
+            // CPU
+            // 
+            this.CPU.HeaderText = "CPU";
+            this.CPU.Name = "CPU";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "PID";
+            this.Column2.Name = "Column2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(495, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(302, 173);
+            this.listBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Papyrus", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label1.Location = new System.Drawing.Point(1, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(469, 46);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Running processes on the machine";
+            // 
+            // commentBox
+            // 
+            this.commentBox.BackColor = System.Drawing.SystemColors.Control;
+            this.commentBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.commentBox.Font = new System.Drawing.Font("Papyrus", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.commentBox.ForeColor = System.Drawing.Color.Red;
+            this.commentBox.Location = new System.Drawing.Point(495, 263);
+            this.commentBox.Multiline = true;
+            this.commentBox.Name = "commentBox";
+            this.commentBox.Size = new System.Drawing.Size(302, 169);
+            this.commentBox.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Papyrus", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label2.Location = new System.Drawing.Point(489, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 33);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Comments:";
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.Silver;
+            this.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.saveButton.Font = new System.Drawing.Font("Papyrus", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.saveButton.Location = new System.Drawing.Point(713, 440);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(84, 23);
+            this.saveButton.TabIndex = 10;
+            this.saveButton.Text = "SAVE";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(828, 491);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.commentBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.processTable);
+            this.Controls.Add(this.closeButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.processTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.DataGridView processTable;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox commentBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CPU;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
+
---
+++ ProcessNote/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace ProcessNote
+{
+    public partial class Form1 : Form
+    {
+        Process[] processlist = Process.GetProcesses();
+        public static Dictionary<int, string> comments = new Dictionary<int, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            int i = 0;
+            foreach (Process process in processlist)
+            {
+                processTable.Rows.Add("", "", "");
+                processTable.Rows[i].Cells[0].Value = process.ProcessName;
+                processTable.Rows[i].Cells[1].Value = process.Id;
+                i++;
+            }
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void processTable_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            listBox1.Items.Clear();
+            int id = processTable.CurrentCell.RowIndex;
+            int processId = Int32.Parse(processTable.Rows[id].Cells[1].Value.ToString());
+            foreach (Process process in processlist) {
+                if (processId == process.Id) {
+                    listBox1.Items.Add("Details: \n");
+                    var content = processTable.CurrentCell.Value.ToString();
+                    listBox1.Items.Add(content);
+                    try {
+                        listBox1.Items.Add("Process Id: " + process.Id);
+                        listBox1.Items.Add("Process StartTime: " + process.StartTime); //Shows the time the process started
+                        listBox1.Items.Add("Process StartInfo: " + process.StartInfo);
+                        listBox1.Items.Add("Process Threads: " + process.Threads); //Gives access to the collection of threads in the process
+                        listBox1.Items.Add("Process Threads Count: " + process.Threads.Count);
+                        listBox1.Items.Add("Physical Memory Usage: " + process.WorkingSet.ToString());
+                        
+                    } catch (Win32Exception)
+                    {
+                        MessageBox.Show("You don't have access!");
+                    }
+                    try {
+                        listBox1.Items.Add("Process UserProcessorTime: " + process.UserProcessorTime);
+                        listBox1.Items.Add("Process TotalProcessorTime: " + process.TotalProcessorTime); //Shows the amount of CPU time the process has taken
+                    } catch (Win32Exception)
+                    {
+                        MessageBox.Show("You don't have access!");
+                    }
+                }
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            int id = processTable.CurrentCell.RowIndex;
+            comments.Add(id, commentBox.Text);
+        }
+    }
+}
